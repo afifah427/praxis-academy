c3352ec32c5bfd3cccf3dde7aa9770f12a3da548
iafifah427 <iafifah427@gmail.com>
2022-08-27 14:59:28 +0700

    novice

---
+++ novice/02-05/7.2.2.Json.py
@@ -0,0 +1,7 @@
+import json
+x = [1, 'simple', 'list']
+f = ['satu', 'dua']
+json.dumps(x)
+print (type (x))
+
+
---
+++ novice/02-05/8.2.Exceptions.py
@@ -0,0 +1,16 @@
+try:
+    print(4 +spam*3)
+except Exception as e:
+    print (e) 
+    
+
+try:
+    print(10 * (1/0))
+except Exception as e:
+    print (e) 
+    
+try:
+    print('2' + 2)
+except Exception as e:
+    print (e) 
+    
