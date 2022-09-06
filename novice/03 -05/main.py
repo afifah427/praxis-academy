b25e2df51e3d3c47f24a26a7de039b7558e55c25
iafifah427 <iafifah427@gmail.com>
2022-09-06 11:40:22 +0700

    novice

--- novice/03 -05/main.py
+++ novice/03 -05/main.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect
 import psycopg2
 app = Flask(__name__)
@@ -25,7 +26,7 @@ def index():
         # print(20*"=")
     
     print(request.method)
-    query = f"select * from buah"
+    query = f"select * from buah order by id desc"
     curs.execute(query)
     data = curs.fetchall()
     curs.close()
@@ -49,6 +50,7 @@ def detail(buah_id):
     conn.close()
     return render_template("detail.html", context=data)
 
+
 @app.route("/delete/<buah_id>")
 def delete(buah_id):
     conn = psycopg2.connect(
@@ -64,6 +66,30 @@ def delete(buah_id):
     curs.close()
     conn.close()
     return redirect("/")
+
+@app.route("/update/<buah_id>", methods=["GET", "POST"])
+def update(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="I270302A"
+    )
+    curs = conn.cursor()
+    if request.method == "POST":
+        nama =request.form.get("nama")
+        detail =request.form.get("detail")
+        query = f"update buah set nama = '{nama}', detail = '{detail}' where id = {buah_id}"
+        curs.execute(query)
+        conn.commit()
+        return redirect("/")
+        
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    data = curs.fetchone()
+    curs.close()
+    conn.close()
+    return render_template("update.html", context=data)
 
 if __name__ == "__main__":
     app.run()
